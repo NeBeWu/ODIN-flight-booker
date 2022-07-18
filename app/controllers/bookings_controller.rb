8da9a850137636531299f5e0581e93605c6b9020
NeBeWu <nebewu@gmail.com>
2022-07-18 20:10:39 -0300

    Setup booking show action and view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,8 @@
 class BookingsController < ApplicationController
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
   def new
     @new_booking = Booking.new(booking_params)
 
@@ -9,6 +13,8 @@ class BookingsController < ApplicationController
 
   def create
     @new_booking = Booking.create(booking_params)
+
+    redirect_to "/bookings/#{@new_booking.id}", notice: 'Flight booked.'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'flights', to: 'flights#index'
   get 'bookings/new', to: 'bookings#new'
   post 'bookings', to: 'bookings#create'
+  get 'bookings/(:id)', to: 'bookings#show'
 
   # Defines the root path route ("/")
   root 'flights#index'
