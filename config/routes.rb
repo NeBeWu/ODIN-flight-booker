6104fa4acdb79d5489bcfb4a750583aaf268ec69
NeBeWu <nebewu@gmail.com>
2022-07-17 22:48:21 -0300

    Setup booking controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'flights', to: 'flights#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'flights', to: 'flights#index'
+  get 'booking/new'
+  get 'booking/create'
 
   # Defines the root path route ("/")
   root 'flights#index'
