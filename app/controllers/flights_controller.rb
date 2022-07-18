4ad27a379dbad5fe7610c5b97e62a2f7a0b59764
NeBeWu <nebewu@gmail.com>
2022-07-17 21:45:15 -0300

    Implement form to select flight

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,11 +4,8 @@ class FlightsController < ApplicationController
   def index
     return if params[:flight].nil?
 
-    @flights = Flight.joins(:departure_airport, :arrival_airport)
-                     .where('airports.location = ?', params[:flight][:departure_airport])
-                     .where('arrival_airports_flights.location = ?', params[:flight][:arrival_airport])
-                     .where('flights.start_datetime::date = ?', params[:flight][:flight_date])
-                     .includes(%i[departure_airport arrival_airport])
+    query_flights
+    @number_passengers = flight_params[:number_passengers]
   end
 
   private
@@ -26,5 +23,18 @@ class FlightsController < ApplicationController
   def set_flight_dates
     @flight_dates = Flight.select('start_datetime').where('start_datetime >= ?', Time.now.to_date)
                           .order(start_datetime: :asc).distinct.map(&:start_datetime).map(&:to_date)
+  end
+
+  # Only allow a list of trusted parameters through.
+  def flight_params
+    params.require(:flight).permit(:departure_airport, :arrival_airport, :flight_date, :number_passengers)
+  end
+
+  def query_flights
+    @flights = Flight.joins(:departure_airport, :arrival_airport)
+                     .where('airports.location = ?', flight_params[:departure_airport])
+                     .where('arrival_airports_flights.location = ?', flight_params[:arrival_airport])
+                     .where('flights.start_datetime::date = ?', flight_params[:flight_date])
+                     .includes(%i[departure_airport arrival_airport])
   end
 end
