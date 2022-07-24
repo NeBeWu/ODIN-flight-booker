b9428fc4b4b11338515200f96054cd21f4545dce
NeBeWu <nebewu@gmail.com>
2022-07-24 19:16:09 -0300

    Implement confirmation email on booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -14,6 +14,8 @@ class BookingsController < ApplicationController
   def create
     @new_booking = Booking.create(booking_params)
 
+    mail_passengers
+
     redirect_to "/bookings/#{@new_booking.id}", notice: 'Flight booked.'
   end
 
@@ -22,5 +24,12 @@ class BookingsController < ApplicationController
   # Only allow a list of trusted parameters through.
   def booking_params
     params.require(:booking).permit(:flight_id, :number_passengers, passengers_attributes: %i[id name email])
+  end
+
+  # Mail each passenger on booking creation
+  def mail_passengers
+    @new_booking.passengers.each do |passenger|
+      PassengerMailer.with(passenger:, booking: @new_booking).booking_confirmation.deliver_later
+    end
   end
 end
--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,10 @@
 class PassengerMailer < ApplicationMailer
+  def booking_confirmation
+    @passenger = params[:passenger]
+    @booking = params[:booking]
+    @flight = @booking.flight
+
+    mail(to: email_address_with_name(@passenger.email, @passenger.name),
+         subject: 'Booking confirmation')
+  end
 end
